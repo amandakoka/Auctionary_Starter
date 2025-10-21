e733c61f916596ec818b7a4507531a44a498c30c
Amanda Koka <amandakoka@Amandas-MacBook-Air.local>
2025-10-21 12:00:40 +0100

    Add question routes

---
+++ app/routes/question.server.routes.js
@@ -0,0 +1,9 @@
+module.exports = function(app) {
+
+    app.route("/item/:itemId/question")
+        .get(question.get_questions_for_item)
+        .post(question.ask_question_for_item);
+    
+    app.route ("/question/:questionId")
+        .post(question.answer_for_question);
+}
