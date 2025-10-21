7a5bf0206687d255b680d829d5d6d54ee84fd4ae
Amanda Koka <amandakoka@Amandas-MacBook-Air.local>
2025-10-21 12:08:48 +0100

    Add controllers to auction and questions file

---
+++ app/controllers/auction.server.controllers.js
@@ -0,0 +1,27 @@
+const search_auctions = (req, res) => {
+    return res.sendStatus(500);
+}
+
+const create_auction = (req, res) => {
+    return res.sendStatus(500);
+}
+
+const get_1_item = (req, res) => {
+    return res.sendStatus(500);
+}
+
+const place_bid = (req, res) => {
+    return res.sendStatus(500);
+}
+
+const get_bids_for_item = (req, res) => {
+    return res.sendStatus(500);
+}
+
+module.exports = {
+    search_auctions: search_auctions,
+    create_auction: create_auction,
+    get_1_item: get_1_item,
+    place_bid: place_bid,
+    get_bids_for_item: get_bids_for_item
+}
---
+++ app/controllers/question.server.controllers.js
@@ -0,0 +1,17 @@
+const get_questions_for_item = (req, res) => {
+    return res.sendStatus(500);
+}
+
+const ask_question_for_item = (req, res) => {
+    return res.sendStatus(500);
+}
+
+const answer_for_question = (req, res) => {
+    return res.sendStatus(500);
+}
+
+module.exports = {
+    get_questions_for_item: get_questions_for_item,
+    ask_question_for_item: ask_question_for_item,
+    answer_for_question: answer_for_question
+}
